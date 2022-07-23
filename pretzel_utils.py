03d5f2779b7353352668292a6a565c4fb19dc436
Christian Wetzel <christian.m.wetzel@gmail.com>
2022-07-23 17:51:50 -0400

    added functions to handle displaying lists

--- pretzel_utils.py
+++ pretzel_utils.py
@@ -3,6 +3,7 @@ import json
 from csv import DictReader, DictWriter
 from datetime import date
 import re
+import math
 
 date_fmt = re.compile(r"\d\d\d\d-\d\d-\d\d")
 
@@ -139,3 +140,57 @@ def load_date_as_obj(o):
         return date.strftime(o)
 
     return o
+
+
+def get_longest_word_from_list(list_of_words):
+    # this will return the longest word within a list of strings
+    longest_word = ""
+    for word in list_of_words:
+        if len(word) > len(longest_word):
+            longest_word = word
+
+    return longest_word
+
+
+def display_list_in_console(data, align="left", columns=4, width=8):
+    # use this to primarily display lists of strings
+    # width can be set by the user, but if it is not a larger amount
+    # then the longest word in the list, then it would be ignored
+    rows = math.ceil(len(data) / columns)
+    start = 0
+    end = columns + 1
+
+    if align == "left":
+        row_format = "{:<{width}}" * (columns - 1) + "{:<}"
+    elif align == "center":
+        row_format = "{:^{width}}" * (columns - 1) + "{:^}"
+    elif align == "right":
+        row_format = "{:>{width}}" * (columns - 1) + "{:>}"
+
+    # this is used to determine the longest word in the list
+    # and dynamically change the spacing to fit the words neatly, with 
+    # enough padding in between the columns
+    longest_word_length = len(get_longest_word_from_list(data))
+
+    if longest_word_length > width:
+        # the width will always be divisible by 4 (the length of a tab 
+        # character, or four spaces)
+        # if the modulo operator returns 0, it will add padding anyways
+        width = longest_word_length + (4 - (longest_word_length % 4))
+
+    for x in range(rows):
+        if len(data[start:]) < columns:
+            row = data[start:]
+        else:
+            row = data[start:end]
+
+        # this will fill in any rows that do not reach the
+        # same length as the desired columns
+        if len(row) < columns:
+            for y in range(columns - len(row)):
+                row.append("")
+        
+        print(row_format.format(*row, width=width))
+
+        start = end - 1
+        end += columns
