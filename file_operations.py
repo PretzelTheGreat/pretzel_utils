a98431d9666dffb529d7a6f64b77390f4d878a2e
Christian Wetzel <christian.m.wetzel@gmail.com>
2023-08-06 14:39:29 -0400

    added encoding as an argument to all the load/save functions

--- file_operations.py
+++ file_operations.py
@@ -3,10 +3,10 @@ import json
 from csv import DictReader, DictWriter
 
 # Loading Files
-def load_json_file(filename, object_hook=None, object_pairs_hook=None):
+def load_json_file(filename, encoding='utf-8', object_hook=None, object_pairs_hook=None):
     data = None
 
-    with open(filename, 'r') as open_file:
+    with open(filename, 'r', encoding=encoding) as open_file:
         if object_hook != None and object_pairs_hook == None:
             data = json.load(open_file, object_hook=object_hook)
         elif object_pairs_hook != None:
@@ -17,10 +17,10 @@ def load_json_file(filename, object_hook=None, object_pairs_hook=None):
     return data
 
 
-def load_csv_file(filename):
+def load_csv_file(filename, encoding='utf-8'):
     data = []
 
-    with open(filename, 'r') as open_file:
+    with open(filename, 'r', encoding=encoding) as open_file:
         csvReader = DictReader(open_file)
 
         for row in csvReader:
@@ -29,17 +29,17 @@ def load_csv_file(filename):
     return data
 
 
-def load_txt_file(filename):
+def load_txt_file(filename, encoding='utf-8'):
     data = []
 
-    with open(filename, 'r') as open_file:
+    with open(filename, 'r', encoding=encoding) as open_file:
         for line in open_file.readlines():
             data.append(line.strip())
 
     return data
 
 
-def load_file(filename, object_hook=None, object_pairs_hook=None):
+def load_file(filename, encoding='utf-8', object_hook=None, object_pairs_hook=None):
     # generic function for importing files
     # file will be imported based on it's extention, with non-extention files being ignored
     # filename 
@@ -56,59 +56,59 @@ def load_file(filename, object_hook=None, object_pairs_hook=None):
     file_ext = os.path.basename(filename).split('.')[1]
 
     if file_ext == 'json':
-        return load_json_file(filename, object_hook=object_hook, object_pairs_hook=object_pairs_hook)
+        return load_json_file(filename, encoding=encoding, object_hook=object_hook, object_pairs_hook=object_pairs_hook)
 
     elif file_ext == 'csv':
-        return load_csv_file(filename)
+        return load_csv_file(filename, encoding=encoding)
 
     elif file_ext == 'txt':
-        return load_txt_file(filename)
+        return load_txt_file(filename, encoding=encoding)
 
     else:
         return None
 
 
 # Saving Files
-def save_json_file(filename, data, custom_encoder=None):
-    with open(filename, 'w') as open_file:
+def save_json_file(filename, data, encoding='utf-8', custom_encoder=None):
+    with open(filename, 'w', encoding=encoding) as open_file:
         if custom_encoder != None:
             json.dump(data, open_file, indent=4, cls=custom_encoder)
         else:
             json.dump(data, open_file, indent=4)
 
 
-def save_csv_file(filename, data, fieldnames):
-    with open(filename, 'w', newline='') as open_file:
+def save_csv_file(filename, data, fieldnames, encoding='utf-8'):
+    with open(filename, 'w', newline='', encoding=encoding) as open_file:
         csvWriter = DictWriter(open_file, fieldnames=fieldnames)
         csvWriter.writeheader()
         for row in data:
             csvWriter.writerow(row)
 
 
-def save_txt_file(filename, data):
-    with open(filename, 'w') as open_file:
+def save_txt_file(filename, data, encoding='utf-8'):
+    with open(filename, 'w', encoding=encoding) as open_file:
         for line in data:
             open_file.write(f"{line}\n")
 
 
-def save_file(filename, data, fieldnames=[], custom_encoder=None):
+def save_file(filename, data, encoding='utf-8', fieldnames=[], custom_encoder=None):
     filename = os.path.normpath(filename)
 
     file_ext = os.path.basename(filename).split('.')[1]
 
     if file_ext == 'json':
         if custom_encoder != None:
-            save_json_file(filename, data, custom_encoder=custom_encoder)
+            save_json_file(filename, data, encoding=encoding, custom_encoder=custom_encoder)
         else:
-            save_json_file(filename, data, custom_encoder)
+            save_json_file(filename, data, custom_encoder, encoding=encoding)
 
     elif file_ext == 'csv':
         if fieldnames == []:
             if type(data) == list:
                 fieldnames = data[0].keys()
-            save_csv_file(filename, data, fieldnames)
+            save_csv_file(filename, data, fieldnames, encoding=encoding)
         else:
-            save_csv_file(filename, data, fieldnames)
+            save_csv_file(filename, data, fieldnames, encoding=encoding)
 
     elif file_ext == 'txt':
-        save_txt_file(filename, data)
+        save_txt_file(filename, data, encoding=encoding)
